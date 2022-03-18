11f9a65a6a683a0255493862a727f2121d697ec3
itaispiegel <itai.spiegel@gmail.com>
2022-03-18 16:01:18 +0200

    Initial commit

---
+++ datastructure_hw1_avl/__main__.py
@@ -0,0 +1,7 @@
+import sys
+
+from datastructure_hw1_avl.datastructure_hw1_avl import fib
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    print(fib(n))
---
+++ test/test_datastructure_hw1_avl.py
@@ -0,0 +1,11 @@
+from datastructure_hw1_avl.datastructure_hw1_avl import fib
+
+
+def test_fib() -> None:
+    assert fib(0) == 0
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(3) == 2
+    assert fib(4) == 3
+    assert fib(5) == 5
+    assert fib(10) == 55
