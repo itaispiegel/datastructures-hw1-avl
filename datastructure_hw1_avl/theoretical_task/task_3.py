ab5dec1a861084b3d9e3213eb79afd7db51ee6a2
itaispiegel <itai.spiegel@gmail.com>
2022-04-16 19:27:07 +0300

    Fix to calculate averages instead of sums

--- datastructure_hw1_avl/theoretical_task/task_3.py
+++ datastructure_hw1_avl/theoretical_task/task_3.py
@@ -1,5 +1,4 @@
 import math
-import statistics
 from typing import Tuple
 
 from datastructure_hw1_avl.avl import AVLTreeList
@@ -16,73 +15,71 @@ class BinarySearchTreeList(AVLTreeList):
         return node.update()
 
 
-def create_arithmetic_progressing_tree_list(size: int) -> Tuple[int, int, int, int]:
+def create_arithmetic_progressing_tree_list(size: int) -> Tuple[float, float, float, float]:
     avl_tree_list, non_avl_tree_list = AVLTreeList(), BinarySearchTreeList()
     avl_operations, non_avl_operations = 0, 0
-    avl_depths, non_avl_depths = [], []
-
+    avl_depths_sum, non_avl_depths_sum = 0, 0
     for i in range(size):
         value = str(i)
         avl_operations += avl_tree_list.insert(0, value)
         inserted_node = avl_tree_list.get(avl_tree_list.search(value) + 1)
-        avl_depths.append(inserted_node.depth())
+        avl_depths_sum += inserted_node.depth()
 
         non_avl_operations += non_avl_tree_list.insert(0, value)
         with set_recursion_limit(2 * size):  # Multiply by 2 just to make sure it's okay
             inserted_node = non_avl_tree_list.get(non_avl_tree_list.search(value) + 1)
-        non_avl_depths.append(inserted_node.depth())
+        non_avl_depths_sum += inserted_node.depth()
     return (
-        avl_operations,
-        non_avl_operations,
-        statistics.mean(avl_depths),
-        statistics.mean(non_avl_depths),
+        avl_operations / size,
+        non_avl_operations / size,
+        avl_depths_sum / size,
+        non_avl_depths_sum / size,
     )
 
 
-def create_balanced_tree_list(size: int) -> Tuple[int, int, int, int]:
+def create_balanced_tree_list(size: int) -> Tuple[float, float, float, float]:
     avl_tree_list, non_avl_tree_list = AVLTreeList(), BinarySearchTreeList()
     avl_operations, non_avl_operations = 0, 0
-    avl_depths, non_avl_depths = [], []
+    avl_depths_sum, non_avl_depths_sum = 0, 0
     for i in range(1, size + 1):
         value = str(i)
         index = 2 * i - 2 ** math.floor(math.log2(2 * i))
 
         avl_operations += avl_tree_list.insert(index, value)
         inserted_node = avl_tree_list.get(avl_tree_list.search(value) + 1)
-        avl_depths.append(inserted_node.depth())
+        avl_depths_sum += inserted_node.depth()
 
         non_avl_operations += non_avl_tree_list.insert(index, value)
         inserted_node = non_avl_tree_list.get(non_avl_tree_list.search(value) + 1)
-        non_avl_depths.append(inserted_node.depth())
+        non_avl_depths_sum += inserted_node.depth()
     return (
-        avl_operations,
-        non_avl_operations,
-        statistics.mean(avl_depths),
-        statistics.mean(non_avl_depths),
+        avl_operations / size,
+        non_avl_operations / size,
+        avl_depths_sum / size,
+        non_avl_depths_sum / size,
     )
 
 
-def create_random_tree_list(size: int) -> Tuple[int, int, int, int]:
+def create_random_tree_list(size: int) -> Tuple[float, float, float, float]:
     avl_tree_list, non_avl_tree_list = AVLTreeList(), BinarySearchTreeList()
     avl_operations, non_avl_operations = 0, 0
-    avl_depths, non_avl_depths = [], []
-
+    avl_depths_sum, non_avl_depths_sum = 0, 0
     for i in range(size):
         value = str(i)
         avl_operations += insert_item_to_random_index_in_tree_list(avl_tree_list, value)
         inserted_node = avl_tree_list.get(avl_tree_list.search(value) + 1)
-        avl_depths.append(inserted_node.depth())
+        avl_depths_sum += inserted_node.depth()
 
         non_avl_operations += insert_item_to_random_index_in_tree_list(
             non_avl_tree_list, value
         )
         inserted_node = non_avl_tree_list.get(non_avl_tree_list.search(value) + 1)
-        non_avl_depths.append(inserted_node.depth())
+        non_avl_depths_sum += inserted_node.depth()
     return (
-        avl_operations,
-        non_avl_operations,
-        statistics.mean(avl_depths),
-        statistics.mean(non_avl_depths),
+        avl_operations / size,
+        non_avl_operations / size,
+        avl_depths_sum / size,
+        non_avl_depths_sum / size,
     )
 
 
@@ -90,62 +87,62 @@ def main():
     for i, size in enumerate(SIZES):
         # ---- 3.1 ----
         (
-            avl_operations,
-            non_avl_operations,
+            avl_avg_operations,
+            non_avl_avg_operations,
             avl_avg_depth,
             non_avl_avg_depth,
         ) = create_arithmetic_progressing_tree_list(size)
         print(
-            f"{avl_operations} operations done (size={size}, index={i + 1}, type=arithmetic_avl)"
+            f"{avl_avg_operations:.3f} operations done (size={size}, index={i + 1}, type=arithmetic_avl)"
         )
         print(
-            f"{non_avl_operations} operations done (size={size}, index={i + 1}, type=arithmetic_non_avl)"
+            f"{non_avl_avg_operations:.3f} operations done (size={size}, index={i + 1}, type=arithmetic_non_avl)"
         )
         print(
-            f"{avl_avg_depth} average depth (size={size}, index={i + 1}, type=arithmetic_avl)"
+            f"{avl_avg_depth:.3f} average depth (size={size}, index={i + 1}, type=arithmetic_avl)"
         )
         print(
-            f"{non_avl_avg_depth} average_depth done (size={size}, index={i + 1}, type=arithmetic_non_avl)"
+            f"{non_avl_avg_depth:.3f} average_depth done (size={size}, index={i + 1}, type=arithmetic_non_avl)"
         )
 
         # ---- 3.2 ----
         (
-            avl_operations,
-            non_avl_operations,
+            avl_avg_operations,
+            non_avl_avg_operations,
             avl_avg_depth,
             non_avl_avg_depth,
         ) = create_balanced_tree_list(size)
         print(
-            f"{avl_operations} operations done (size={size}, index={i + 1}, type=balanced_avl)"
+            f"{avl_avg_operations:.3f} operations done (size={size}, index={i + 1}, type=balanced_avl)"
         )
         print(
-            f"{non_avl_operations} operations done (size={size}, index={i + 1}, type=balanced_non_avl)"
+            f"{non_avl_avg_operations:.3f} operations done (size={size}, index={i + 1}, type=balanced_non_avl)"
         )
         print(
-            f"{avl_avg_depth} average depth (size={size}, index={i + 1}, type=balanced_avl)"
+            f"{avl_avg_depth:.3f} average depth (size={size}, index={i + 1}, type=balanced_avl)"
         )
         print(
-            f"{non_avl_avg_depth} average_depth done (size={size}, index={i + 1}, type=balanced_non_avl)"
+            f"{non_avl_avg_depth:.3f} average_depth done (size={size}, index={i + 1}, type=balanced_non_avl)"
         )
 
         # ---- 3.3 ----
         (
-            avl_operations,
-            non_avl_operations,
+            avl_avg_operations,
+            non_avl_avg_operations,
             avl_avg_depth,
             non_avl_avg_depth,
         ) = create_random_tree_list(size)
         print(
-            f"{avl_operations} operations done (size={size}, index={i + 1}, type=random_avl)"
+            f"{avl_avg_operations:.3f} operations done (size={size}, index={i + 1}, type=random_avl)"
         )
         print(
-            f"{non_avl_operations} operations done (size={size}, index={i + 1}, type=random_non_avl)"
+            f"{non_avl_avg_operations:.3f} operations done (size={size}, index={i + 1}, type=random_non_avl)"
         )
         print(
-            f"{avl_avg_depth} average depth (size={size}, index={i + 1}, type=random_avl)"
+            f"{avl_avg_depth:.3f} average depth (size={size}, index={i + 1}, type=random_avl)"
         )
         print(
-            f"{non_avl_avg_depth} average_depth done (size={size}, index={i + 1}, type=random_non_avl)"
+            f"{non_avl_avg_depth:.3f} average_depth done (size={size}, index={i + 1}, type=random_non_avl)"
         )
         print()
 
